4de4f33eba6c4f06814c5aabc3709909e59dce58
Mark Baggett <mbagget1@utk.edu>
2022-08-23 08:52:50 -0400

    Generate ontology information.

--- utilities/query_role.py
+++ utilities/query_role.py
@@ -1,6 +1,7 @@
 import requests
-from rdflib import Graph, URIRef, RDFS, Literal
-from rdflib.namespace import RDF, SKOS, DC, RDFS
+from rdflib import Graph, URIRef, RDFS, Literal, XSD
+from rdflib.namespace import RDF, SKOS, DC, RDFS, DCTERMS, OWL
+import arrow
 
 
 class RoleBuilder:
@@ -20,10 +21,6 @@ class RoleBuilder:
         full_graph = Graph().parse(data=self.data, format="json-ld")
         subject = f"https://ontology.lib.utk.edu/roles#{self.uri.split('/')[-1]}"
         g = Graph()
-        g.bind("skos", SKOS)
-        g.bind("utk", "https://ontology.lib.utk.edu/roles#")
-        g.bind("dc", "http://purl.org/dc/elements/1.1/")
-        g.bind("relators", "http://id.loc.gov/vocabulary/relators/")
         g.add((URIRef(subject), RDF.type, RDF.Property))
         for s, p, o in full_graph:
             if p.strip() == "http://www.w3.org/2004/02/skos/core#prefLabel" and s.strip() == self.uri:
@@ -33,6 +30,7 @@ class RoleBuilder:
         g.add((URIRef(subject), SKOS.closeMatch, URIRef(self.uri)))
         g.add((URIRef(subject), RDFS.range, RDFS.Literal))
         g.add((URIRef(subject), RDFS.subPropertyOf, DC.contributor))
+        g.add((URIRef(subject), RDFS.isDefinedBy, URIRef("https://ontology.lib.utk.edu/roles#")))
         return g
 
 
@@ -45,17 +43,34 @@ class OntologyGenerator:
         g.bind("skos", SKOS)
         g.bind("utk", "https://ontology.lib.utk.edu/roles#")
         g.bind("dc", "http://purl.org/dc/elements/1.1/")
+        g.bind("dcterms", "http://purl.org/dc/terms/")
         g.bind("relators", "http://id.loc.gov/vocabulary/relators/")
+        g.bind("xsd", "http://www.w3.org/2001/XMLSchema#")
         with open(input, 'r') as my_input:
             for line in my_input:
-                role = RoleBuilder(line.strip()).build()
-                for thing in role:
-                    g.add(thing)
+                role_graph = RoleBuilder(line.strip()).build()
+                for role in role_graph:
+                    g.add(role)
+        ontology = self.add_ontology_information()
+        for triple in ontology:
+            g.add(triple)
         with open(f"ontologies/{self.filename}", "wb") as rdf:
             rdf.write(
                 g.serialize(format='turtle', indent=4).encode("utf-8")
             )
         return
+
+    def add_ontology_information(self):
+        g = Graph()
+        date = arrow.utcnow().format('YYYY-MM-DD')
+        comment = "Ontology for the the University of Tennessee Libraries to state relationships between people and works.  This ontology exists because m3 profiles defined by Houndstooth cannot support both URIs and strings.  For that reason, this ontology exists and closely follows marcrelators.  The key difference is that this ontology is defined to work with values that are strings only."
+        g.add((URIRef('https://ontology.lib.utk.edu/roles#'), DCTERMS.modified, Literal(date, datatype=XSD.date)))
+        g.add((URIRef('https://ontology.lib.utk.edu/roles#'), RDFS.comment, Literal(comment, lang='en')))
+        g.add((URIRef('https://ontology.lib.utk.edu/roles#'), DCTERMS.publisher, URIRef('http://www.lib.utk.edu/')))
+        g.add((URIRef('https://ontology.lib.utk.edu/roles#'), DCTERMS.title, Literal('University of Tennessee Digital Initiatives Role Terms Ontology', lang='en')))
+        g.add((URIRef('https://ontology.lib.utk.edu/roles#'), RDFS.seeAlso, URIRef('https://github.com/utkdigitalinitiatives/utk_ontology/')))
+        g.add((URIRef('https://ontology.lib.utk.edu/roles#'), OWL.versionInfo, Literal(date, datatype=XSD.date)))
+        return g
 
 
 if __name__ == "__main__":
