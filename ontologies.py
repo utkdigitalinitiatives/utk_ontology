c1c71858d46cedb0eddafc12913f936723e0bcb4
Mark Baggett <mbagget1@utk.edu>
2022-12-06 09:37:21 -0500

    Remove extra route in controller.

--- ontologies.py
+++ ontologies.py
@@ -21,31 +21,13 @@ def index():
     )
 
 
-@app.route('/roles')
 @app.route('/<path:path>')
 @returns_rdf
 def roles(path=''):
     g = Graph()
-    g.parse("ontologies/roles.ttl", format='ttl')
+    g.parse(f"ontologies/{path}.ttl", format='ttl')
     if flask_rdf.wants_rdf(request.headers['Accept']) is False:
-        ontology = htmlify.OntologyCleaner(graph=g, namespace="https://ontology.lib.utk.edu/roles#")
-        return render_template(
-            'ontology.html',
-            details=ontology.ontology_details,
-            classes_and_props=ontology.properties_and_classes,
-            namespaces=ontology.all_namespaces
-        )
-    else:
-        return g
-
-
-@app.route('/works')
-@returns_rdf
-def files(path=''):
-    g = Graph()
-    g.parse("ontologies/works.ttl", format='ttl')
-    if flask_rdf.wants_rdf(request.headers['Accept']) is False:
-        ontology = htmlify.OntologyCleaner(graph=g, namespace="https://ontology.lib.utk.edu/works#")
+        ontology = htmlify.OntologyCleaner(graph=g, namespace=f"https://ontology.lib.utk.edu/{path}#")
         return render_template(
             'ontology.html',
             details=ontology.ontology_details,
