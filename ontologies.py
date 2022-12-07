0192548221d04905052126cafebfa5d0531d8f22
Mark Baggett <mbagget1@utk.edu>
2022-12-07 13:05:49 -0500

    Init addressing merge conflict.

--- ontologies.py
+++ ontologies.py
@@ -41,18 +41,17 @@ def index():
 @app.route('/<path:path>')
 @returns_rdf
 def roles(path=''):
-    g = Graph()
-    g.parse(f"ontologies/{path}.ttl", format='ttl')
-    if flask_rdf.wants_rdf(request.headers['Accept']) is False:
-        ontology = htmlify.OntologyCleaner(graph=g, namespace=f"https://ontology.lib.utk.edu/{path}#")
-        return render_template(
-            'ontology.html',
-            details=ontology.ontology_details,
-            classes_and_props=ontology.properties_and_classes,
-            namespaces=ontology.all_namespaces
-        )
-    else:
-        return g
+  g = Graph()
+  g.parse("ontologies/roles.ttl", format='ttl')
+  return g
+
+
+@app.route('/works')
+@returns_rdf
+def files(path=''):
+  g = Graph()
+  g.parse("ontologies/works.ttl", format='ttl')
+  return g
 
 
 if __name__ == '__main__':
